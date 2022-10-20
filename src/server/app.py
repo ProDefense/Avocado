64d9f7c9e033cf18b627e7b451b467ead59d5a64
Fahad <fahad@Fahads-MBP.local>
2022-10-19 17:19:37 -0700

    Recreated the main window using QT Designer and added a simple menu bar

--- src/server/app.py
+++ src/server/app.py
@@ -1,19 +1,24 @@
+#!/usr/bin/env python3
+
 import os
 
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow
 import sys
 from PyQt6.QtGui import QIcon
+from main_window import Ui_MainWindow
 
-class Window(QWidget):
+class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Avocado C2")
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
 app = QApplication(sys.argv)
 app.setApplicationName("Avocado")
-path = os.path.join(os.path.dirname(sys.modules[__name__].__file__), 'assets/avocadologo.png')
+path = os.path.join(os.path.dirname(sys.modules[__name__].__file__), "assets/avocadologo.png")
 app.setWindowIcon(QIcon(path))
+
 window = Window()
 window.show()
-sys.exit(app.exec())
 
+sys.exit(app.exec())
