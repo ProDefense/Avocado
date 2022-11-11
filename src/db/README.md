20e59c36fb2522be8096d8c08ec3e05ad2d888e1
j-dwo <jacqueline.dwor@gmail.com>
2022-11-11 11:35:45 -0800

    Began development of postgres database>

--- src/db/README.md
+++
@@ -1,1 +0,0 @@
-make_db.py creates tables in the local database test_db. To make database, run the following command: python3 make_db.py.
--- src/db/sqlite/make_db.py
+++ src/db/sqlite/make_db.py
@@ -35,11 +35,17 @@ lootRecords = """ CREATE TABLE IF NOT EXISTS Loot (
                   [Loot_UUID] STRING PRIMARY KEY,
                   [Loot_Type] TEXT,
                   [Implant_UUID] TEXT,
-                  [CreatedAt] NUMERIC)"""           
+                  [CreatedAt] NUMERIC)""" 
+
+operatorRecords = """ CREATE TABLE Operators (
+                  [User] STRING PRIMARY KEY,
+                  [Password] STRING,
+                  [Public_Key] STRING)"""       # not decided if we are going with password or public key based       
 
 # create tables in db
 c2Cursor.execute(implantRecords)
 c2Cursor.execute(lootRecords)
+c2Cursor.execute(operatorRecords)
 
 # close database
 c2db.close()
