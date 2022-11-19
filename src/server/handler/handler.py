3e7a7359d6f137a88a3ef13d80dc4aa4f0b87c7c
j-dwo <jacqueline.dwor@gmail.com>
2022-11-18 17:47:08 -0800

    Drafted code to add implent to the database. As of now, unable to run handler.py to test this code.

--- src/server/handler/handler.py
+++ src/server/handler/handler.py
@@ -2,7 +2,7 @@
 import threading
 from queue import Queue
 from pb import implantpb_pb2
-
+from sqlalchemy import create_engine, engine, insert
 
 class Handler:
     def __init__(self, requestq: Queue):
@@ -11,6 +11,9 @@ class Handler:
     def start(self):
         t = threading.Thread(target=self.__handle, args=())
         t.start()
+
+        # Connect to SQLAlchemy engine
+        engine = create_engine('postgresql+psycopg2://postgres:password@localhost:5432/test_db')
 
     # Handle incoming registrations
     def __handle(self):
@@ -23,8 +26,14 @@ class Handler:
             if len(implant.addr) < 1:
                 implant.addr = str(addr)
 
-            # TODO: Add the `implant` to the database
+            # TODO: Test adding implant to the database. May need to import database classes if this method does not work
+            stmt = insert(Implants).values(OS = implant.os, Arch = "?", IPv4 = "?", Hostname = "?", Username = implant.user.name, PID = implant.pid)
+            with engine.connect() as conn:
+                result = conn.execute(stmt)
+                conn.commit()
 
+            
+i
             # TODO: Log this part here instead of printing to stdout
             display = f"""
             Accepted new connection:
