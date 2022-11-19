b5259657533732d5a62d0c4803bce5d3d30422fa
sadeli413 <sadeli0x19d@gmail.com>
2022-11-19 14:00:04 -0700

    Tidy up this branch to prepare for main

--- src/certs/mkcerts.sh
+++ src/certs/mkcerts.sh
@@ -5,15 +5,11 @@
 CURR=$PWD
 mkdir client
 mkdir server
-mkdir operator
 
 cd $CURR/server
 CAROOT=$PWD mkcert 'avocado-server.c2'
 
 cd $CURR/client
 CAROOT=$PWD mkcert -client 'avocado-implant.c2'
-
-cd $CURR/operator
-CAROOT=$PWD mkcert -client 'avocado-operator.c2'
 
 cd $CURR
