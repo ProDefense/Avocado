ccc3f4050c97230513564ce5256eada87d161bd7
william bowden <williambowden4@gmail.com>
2022-12-13 21:38:13 -0700

    Created new ui file just for remote machines table. Added both remote machines list and active session terminal to one window that can be run in app.py

--- src/server/app.py
+++ src/server/app.py
@@ -4,14 +4,16 @@ import os
 
 from PyQt6 import QtCore, QtWidgets, QtGui
 from PyQt6.QtCore import QAbstractTableModel, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTableView, QStyle
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTableView, QStyle, QVBoxLayout, QWidget
 import sys
 from PyQt6.QtGui import QIcon
+
+from src.server.active_session_logic import ActiveSession
+from src.server.main_window import Ui_MainWindow
+from src.server.remote_machines import Ui_RemoteMachines
+
 DisplayRole = Qt.ItemDataRole.DisplayRole
 Horizontal = Qt.Orientation.Horizontal
-
-
-from main_window import Ui_MainWindow
 
 
 class RemoteMachinesModel(QAbstractTableModel):
@@ -40,27 +42,11 @@ class RemoteMachinesModel(QAbstractTableModel):
             return QtCore.QVariant(self.headers[section])
         return QtCore.QVariant(int(section + 1))
 
-
-# remoteMachineStyleSheet = """
-# QWidget {
-#     background-color: #24273A;
-#     color: "white";
-# }
-# QPushButton {
-#     font-size: 16px;
-#     background-color: "darkblue"
-# }
-# QLineEdit {
-#     background-color: "white";
-#     color: "black";
-# }
-# """
-
-class MainWindow(QMainWindow, Ui_MainWindow):
+#
+class RemoteMachines(QWidget, Ui_RemoteMachines):
 
     def __init__(self):
-        QMainWindow.__init__(self)
-        Ui_MainWindow.__init__(self)
+        super(RemoteMachines, self).__init__()
         self.setupUi(self)
 
         # test data
@@ -84,12 +70,30 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         return remoteMachinesStyleSheet.read()
 
 
+
+# Main app that connects widgets into one window
+class MainApp(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.layout = QVBoxLayout()
+        # add active session and remote machines table to one widget
+        self.layout.addWidget(RemoteMachines())
+        self.layout.addWidget(ActiveSession())
+        # widget holds layout
+        widget = QWidget()
+        widget.setLayout(self.layout)
+        self.setCentralWidget(widget)
+
+
+
 app = QApplication(sys.argv)
 app.setApplicationName("Avocado")
 path = os.path.join(os.path.dirname(sys.modules[__name__].__file__), "assets/avocadologo.png")
 app.setWindowIcon(QIcon(path))
 
-window = MainWindow()
+window = MainApp()
 window.show()
 
 sys.exit(app.exec())
