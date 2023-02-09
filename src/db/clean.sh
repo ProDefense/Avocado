c579facbe84ede43a8c3663a5df4a0808a939008
TopShelfAsian <rjflip@gmail.com>
2023-02-08 17:30:59 -0700

    Finished the clean.sh script; deletes all the database and table space.

--- src/db/clean.sh
+++ src/db/clean.sh
@@ -5,11 +5,5 @@
 
 echo "start"
 sudo -su postgres pg_dump --dbname=test_db > test.txt
-echo "phase1"
-sudo -u postgres -H -- psql -c "DROP DATABASE test_db;"
-exit
-exit
-#cant get this portion to execute properly
-sudo -u postgres -H -- psql -c "DROP tablespace db_tablespace;"
-exit
-exit
+sudo -u postgres -H -- psql -c 'DROP DATABASE test_db;' -c 'DROP tablespace db_tablespace;'
+echo "Exported and cleaned."
