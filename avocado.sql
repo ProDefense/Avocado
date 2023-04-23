543159aca1de869241b822904cf37c3598302f4d
TopShelfAsian <rjflip@gmail.com>
2023-04-23 00:10:39 -0700

    sql file that stands in for make_db.py, still needs to to be tweaked to add tablespace. However does create all the tables needed.

--- avocado.sql
+++ avocado.sql
@@ -1,9 +1,32 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    email VARCHAR(255)
+--CREATE TABLESPACE db
+--    OWNER CURRENT_USER
+--    LOCATION '~/postgres/data';
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE Implants 
+(
+    Implant_UUID UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    OS VARCHAR(64),
+    Arch VARCHAR(64),
+    IPv4 VARCHAR(64),
+    Hostname VARCHAR(64),
+    Username VARCHAR(64),
+    PID INTEGER,
+    ImplantUpTime TIMESTAMP WITH TIME ZONE
 );
 
-INSERT INTO users (name, email) VALUES
-    ('John Doe', 'john.doe@example.com'),
-    ('Jane Doe', 'jane.doe@example.com');
+CREATE TABLE Operators 
+(
+    Operator_UUID UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    "User" VARCHAR(64),
+    Passwrd VARCHAR(64)
+);
+
+CREATE TABLE Loot 
+(
+    Loot_UUID UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    Loot_Type VARCHAR(64),
+    Implant_UUID UUID NOT NULL REFERENCES Implants(Implant_UUID),
+    Operator_UUID UUID NOT NULL REFERENCES Operators(Operator_UUID),
+    CreatedAt TIMESTAMP WITH TIME ZONE
+);
